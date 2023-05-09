0493195ae9fbe7f034c63f173303c2cf8c82a83f
noot <limtiseng@gmail.com>
2023-05-09 13:04:01 +0800

    Add files via upload

---
+++ reboot-shutdown.ps1
@@ -0,0 +1,15 @@
+﻿# Prompt user for input
+$userInput = Read-Host "Please specify the action you wish to take!"
+
+if($userInput -eq "shutdown")
+{
+ Stop-Computer
+}
+elseif ($userInput -eq "restart" )
+{
+ Restart-Computer -Force
+}
+else 
+{
+ Write-Host "Invalid input. Please enter 'shutdown' pr 'restart'"
+}
---
+++ update-checker.bat
@@ -0,0 +1,6 @@
+@echo off
+wmic qfe list
+TIMEOUT /T 20 /nobreak 
+
+
+
